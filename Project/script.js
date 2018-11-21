fee26dced9d99e431b2ba67ac95a8ec4f7cc502e
Kevin Bingham <kbingham@uhokbingham.inside.fender>
2018-11-21 15:22:04 -0800

    test

--- Project/script.js
+++ Project/script.js
@@ -1,6 +1,9 @@
 /* API info */
 const apiKey = "d4fb5f3d39fd411cbb3205304182111";
-const openWeatherUrl = "https://api.apixu.com/v1/current.json?key=";
+const weatherUrl = "https://api.apixu.com/v1/current.json?key=";
+
+const geoApi = "&apikey=e72d98ec79024cd5b1fd1f39f75d0b81&format=json&notStore=false&version=4.10";
+const geoUrl = "https://geoservices.tamu.edu/Services/ReverseGeocoding/WebService/v04_01/Rest/?";
 
 /*Get current location*/
 var latitude = 0;
@@ -10,40 +13,42 @@ var lon;
 
 var currLoc = document.getElementById('currentLocation');
 
-function currentLocation(){
-  if(navigator.geolocation){
+function currentLocation() {
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
-  }else{
+  } else {
     currLoc.innerHTML = "Geolocation is not supported by this browser.";
   }
-  showPosition();
+  formatLoc();
 };
 
 function showPosition(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    formatLoc();
-  };
-
-function round(value, decimals) {
-  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
-};
-
-function formatLoc(){
-  if(latitude != 0 && longitude != 0){
-    lat = round(latitude, 6);
-    lon = round(longitude, 6);
-  }
-  getRequest();
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
 };
 
 currLoc.addEventListener('touchstart', currentLocation);
 currLoc.addEventListener('click', currentLocation);
 
-function getRequest() {
- var url = openWeatherUrl + apiKey +"&q=Phoenix" ;
+function round(value, decimals) {
+  return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+};
+var lat;
+var lon;
+
+function formatLoc() {
+  if (latitude != 0 && longitude != 0) {
+    lat = round(latitude, 2);
+    lon = round(longitude, 2);
+  }
+  getLocRequest();
+};
+
+var objectResponse;
+
+function getWeatherRequest(location) {
+  var url = weatherUrl + apiKey + "&q=" + location;
   var xhr = new XMLHttpRequest();
-  var objectResponse;
   xhr.onreadystatechange = function() {
     if (xhr.readyState == XMLHttpRequest.DONE) {
       objectResponse = JSON.parse(this.responseText);
@@ -53,12 +58,31 @@ function getRequest() {
   }
   xhr.open('GET', url, true);
   xhr.send();
-  console.log(objectResponse);
+  //document.getElementById('locationFav').innerHTML = objectResponse[1];
 };
+
+function getLocRequest() {
+  var url = geoUrl + "lat=" + lat + "&lon=" + lon + geoApi;
+  var locResponse;
+  var location;
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      locResponse = JSON.parse(this.responseText);
+      location = locResponse.StreetAddresses[0].City;
+    } else if (xhr.readyState != XMLHttpRequest.DONE) {
+      console.log('There was an error ' + this.status + this.statusText)
+    }
+  }
+  xhr.open('GET', url, true);
+  xhr.send();
+  getWeatherRequest(location);
+};
+
 /* Search Location */
 var search = document.getElementById('searchBox');
 
-function searchLocation(){
+function searchLocation() {
   currLoc.innerHTML = search.value;
 };
 
